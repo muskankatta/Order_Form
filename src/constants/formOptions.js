90be182ad5ac3c1170f3be441e746466b24c9335
muskankatta <muskankatta2@gofynd.com>
2026-05-05 14:56:47 +0530

    Update formOptions.js

--- src/constants/formOptions.js
+++ src/constants/formOptions.js
@@ -61,7 +61,7 @@ export const SLAB_RATE_UNITS = [
 ];
 
 export const UNIT_METRICS = [
-  ''Bag (Placed)','Bag (Invoiced)','Shipment (Placed)','Order (Placed)','Item (Placed)',
+  'Bag (Placed)','Bag (Invoiced)','Shipment (Placed)','Order (Placed)','Item (Placed)',
   'Store','User','Rider','Warehouse','Location','Item','SKU','Credit',
   'Message (WhatsApp)','Image','Video','Tech Pack','Forward Picked',
   'RTO Picked','Return Picked','BCA (Brand Calculated Amount)',
