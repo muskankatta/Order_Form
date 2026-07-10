27b705bae3e263c2f6aec2171e37f9ab75e4ccca
muskankatta <muskankatta2@gofynd.com>
2026-07-10 13:17:12 +0530

    Update Slack channel ID for India

--- src/utils/slack.js
+++ src/utils/slack.js
@@ -5,7 +5,7 @@
 const BOLTIC_URL = import.meta.env.VITE_BOLTIC_SLACK_URL || '';
 
 const CHANNELS = {
-  India:    'C0978TZNGM8',
+  India:    'C0392LXA3B4',
   Global:   'C09CMHKF231',
   RJW:      'C0978TZNGM8',
 };
