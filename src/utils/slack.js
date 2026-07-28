6c3ab1eceaca18fd53230a5ae6b0305456e1b524
muskankatta <muskankatta2@gofynd.com>
2026-07-28 11:59:59 +0530

    Update Slack channel ID for RJW

--- src/utils/slack.js
+++ src/utils/slack.js
@@ -7,7 +7,7 @@ const BOLTIC_URL = import.meta.env.VITE_BOLTIC_SLACK_URL || '';
 const CHANNELS = {
   India:    'C0392LXA3B4',
   Global:   'C09CMHKF231',
-  RJW:      'C0978TZNGM8',
+  RJW:      'C0B18PJMKJP',
 };
 
 const SLACK_IDS = {
